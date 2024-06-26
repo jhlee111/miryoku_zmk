e3334a726ad6458fe247e4a006b67dfc9dd5f702
jhlee111 <75551846+jhlee111@users.noreply.github.com>
2024-06-26 09:17:20 -0700

    adds custom symbol layer ver 1

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -7,3 +7,19 @@
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, RET),U_LT(U_MOUSE, TAB),U_LT(U_SYM, SPACE),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_LAYER_SYM \
+&kp LPAR,          &kp RPAR,          &kp CARET,         &kp SEMI,          &kp COLON,         &kp MINUS,         &kp AT,            &kp HASH,         &kp PRCNT,        &kp TILDE,            \
+&kp LBRC,          &kp RBRC,          &kp LT,            &kp EQUAL,         &kp GT,            &kp PLUS,          &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp LBKT,          &kp RBKT,          &kp AMPS,          &kp PIPE,          &kp EXCL,          &kp SLASH,         &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+U_NP    ,             U_NP,           &kp DLLR,         &kp UNDER,          &kp ASTRK,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+
+
+
+
+
+
+
+
+
